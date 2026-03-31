f000f626c876a1cd30aedec6332bbaba2de7a7a3
Dovran <dovran.tech@gmail.com>
2026-03-31 18:41:47 +0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿
+class Program
+{
+    static string ReverseString(string str)
+    {
+        var chars = str.ToArray();
+        int left = 0;
+        int right = chars.Length - 1;
+
+        while(left < right)
+        {
+            (chars[left], chars[right]) = (chars[right], chars[left]);
+
+            left++;
+            right--;
+        }
+
+        return new string(chars);
+    }
+
+    static string ReverseStringRecursive(string str)
+    {
+        if(str == "")
+           return str;
+
+        return ReverseString(str.Substring(1)) + str[0];
+    }
+    static void Main(string[] args)
+    {
+    }
+}
